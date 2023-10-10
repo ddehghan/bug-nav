7a63f84e2d0c18b70ac7149a2687b7a5e9d716f9
David Dehghan <ddehghan@gmail.com>
2023-10-09 18:48:08 -0700

    add drawer code

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const HomeScreen = () => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>Home Screen</Text>
+  </View>
+);
+
+const SettingsScreen = () => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>Settings Screen</Text>
+  </View>
+);
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
